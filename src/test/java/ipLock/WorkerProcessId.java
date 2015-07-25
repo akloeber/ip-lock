f350b51f988ebbbe65f9bfc8f7062c804ce64fc2
Andreas Klöber <andreas.kloeber@gmail.com>
2015-07-25 13:15:53 +0200

    WorkerProcessId.next implemented

--- src/test/java/ipLock/WorkerProcessId.java
+++ src/test/java/ipLock/WorkerProcessId.java
@@ -1,9 +1,17 @@
 package ipLock;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Class WorkerProcessID
  *
  * @author Andreas Klöber
  */
-public class WorkerProcessID {
+public class WorkerProcessId {
+
+    private static AtomicInteger idCounter = new AtomicInteger(0);
+
+    public static Integer next() {
+        return idCounter.incrementAndGet();
+    }
 }
